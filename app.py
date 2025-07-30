863f8f4e750f76fc0da5a03178ce224afc66fb20
windesally <peewinmana@gmail.com>
2025-07-30 13:45:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def main():
             else:
                 st.error("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง")
     else:
-        st.snow()
         st.write(f"User: {st.session_state.username}")
         Machine = st.selectbox("Select Machine",('Data','PKASM011', 'PKASM012', 'PKASM013', 'PKASM014', 'PKASM015', 'PKASM016', 'PKASM017', 'PKASM018', 'PKASM019', 'PKASM020', 
                                    'PKASM021', 'PKASM022', 'PKASM023', 'PKASM024', 'PKASM025', 'PKASM026', 'PKASM027', 'PKASM028', 'PKASM029', 'PKASM030', 
